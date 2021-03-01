491090452c7f3b368d549491cf83167ad594c51d
Nicolas Wiedel <nwiedel@alte-hof-apotheke.com>
2021-03-01 22:36:17 +0100

    initial Commit

---
+++ desktop/src/de/javadevblog/myrpg/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package de.javadevblog.myrpg.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import de.javadevblog.myrpg.MyRPG;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MyRPG(), config);
+	}
+}
